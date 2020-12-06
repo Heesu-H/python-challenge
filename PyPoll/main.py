bf0ee8dfd49f7d54e9e00c35acde50bac3406429
Heesu-H <heesu456@hotmail.com>
2020-12-07 01:08:19 +1100

    modified:   main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,15 @@ def PyPoll(electiondata_filelocation):
     file_location = electiondata_filelocation
 
     list_candidates = []
-    candidates_filtered = []
+    filtered = []
+    k_votes = []
+    k_total = 0
+    c_votes = []
+    c_total= 0
+    l_votes = []
+    l_total = 0
+    o_votes = []
+    o_total = 0
     total_votes = 0
 
 
@@ -23,12 +31,31 @@ def PyPoll(electiondata_filelocation):
                 list_candidates.append(row[2])
         
         for candidate in list_candidates:
-            if candidate not in candidates_filtered:
-                candidates_filtered.append(candidate)
+            if candidate not in filtered:
+                filtered.append(candidate)
         #or can conver list to dict and back to list. found at https://www.w3schools.com/python/python_howto_remove_duplicates.asp
         #candidates = list_to_dict(list_candidates)
     #print(candidates)
-    print(candidates_filtered)
+
+        #counting total number of votes for each candidate
+        for votes in list_candidates:
+            if votes == filtered[0]:
+                k_votes.append(votes)
+                k_total +=1
+            if votes == filtered[1]:
+                c_votes.append(votes)
+                c_total+=1
+            if votes == filtered[2]:
+                l_votes.append(votes) 
+                l_total+=1
+            if votes == filtered[3]:
+                o_total+=1
+                o_votes.append(votes)
+
+    print(k_total,c_total,l_total,o_total)
+
+    print(filtered)
+
 
 
 #additional solution to filtering out duplicates
